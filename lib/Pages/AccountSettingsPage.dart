2cf8100567d8a8e23df0cc604067546b6cd2c71b
exarth-dev-malik <ufkeyfans786@gmail.com>
2021-10-03 23:46:11 +0500

    Account Settings Page

--- lib/Pages/AccountSettingsPage.dart
+++ lib/Pages/AccountSettingsPage.dart
@@ -13,7 +13,22 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class Settings extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.lightBlue,
+        title: Text(
+          "Account Settings",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: SettingsScreen(),
+    );
+  }
 }
 
 class SettingsScreen extends StatefulWidget {
@@ -26,13 +41,12 @@ class SettingsScreenState extends State<SettingsScreen> {
   TextEditingController aboutMeTextEditingController;
 
   SharedPreferences preferences;
-  String id= "";
-  String nickname= "";
-  String aboutMe= "";
-  String photoUrl= "";
+  String id = "";
+  String nickname = "";
+  String aboutMe = "";
+  String photoUrl = "";
   File imageFileAvatar;
   bool isLoading = false;
-
 
   @override
   void initState() {
@@ -42,85 +56,103 @@ class SettingsScreenState extends State<SettingsScreen> {
     readDataFromLocal();
   }
 
-  void readDataFromLocal () async{
+  void readDataFromLocal() async {
     preferences = await SharedPreferences.getInstance();
     id = preferences.getString("id");
     nickname = preferences.getString("nickname");
     aboutMe = preferences.getString("aboutMe");
     photoUrl = preferences.getString("photoUrl");
 
-    nickNameTextEditingController =TextEditingController(text: nickname);
-    aboutMeTextEditingController =TextEditingController(text: aboutMe);
+    nickNameTextEditingController = TextEditingController(text: nickname);
+    aboutMeTextEditingController = TextEditingController(text: aboutMe);
 
-    setState(() {
+    setState(() {});
+  }
 
-    });
-    Future getImage() async {
-      File newImageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+  void getImage() async {
+    File newImageFile =
+        await ImagePicker.pickImage(source: ImageSource.gallery);
 
-      if(newImageFile!= null){
-        setState(() {
-          this.imageFileAvatar = newImageFile;
-          isLoading= true;
-        });
-
-      }
+    if (newImageFile != null) {
+      setState(() {
+        this.imageFileAvatar = newImageFile;
+        isLoading = true;
+      });
     }
   }
-  Widget build(BuildContext context) {
 
+  @override
+  Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         SingleChildScrollView(
           child: Column(
             children: <Widget>[
-             // Profile image - Avatar
-      Container(
-        child: Center(
-          child: Stack(
-            children: <Widget>[
-              (imageFileAvatar ==null)
-              ? (photoUrl !="")
-                  ? Material(
-                //display alreday existing old image file
-                child: CachedNetworkImage(
-                  placeholder: (context, url) => Container(
-                    child: CircularProgressIndicator(
-                      strokeWidth: 2.0,
-                      valueColor: AlwaysStoppedAnimation<Color>(Colors.lightBlueAccent),
-                    ),
-                    width: 200.0,
-                    height: 200.0,
-                    padding: EdgeInsets.all(20.0),
+              // Profile image - Avatar
+              Container(
+                child: Center(
+                  child: Stack(
+                    children: <Widget>[
+                      (imageFileAvatar == null)
+                          ? (photoUrl != "")
+                              ? Material(
+                                  //display alreday existing old image file
+                                  child: CachedNetworkImage(
+                                    placeholder: (context, url) => Container(
+                                      child: CircularProgressIndicator(
+                                        strokeWidth: 2.0,
+                                        valueColor:
+                                            AlwaysStoppedAnimation<Color>(
+                                                Colors.lightBlueAccent),
+                                      ),
+                                      width: 200.0,
+                                      height: 200.0,
+                                      padding: EdgeInsets.all(20.0),
+                                    ),
+                                    imageUrl: photoUrl,
+                                    width: 200.0,
+                                    height: 200.0,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(125.0)),
+                                  clipBehavior: Clip.hardEdge,
+                                )
+                              : Icon(
+                                  Icons.account_circle,
+                                  size: 90.0,
+                                  color: Colors.grey,
+                                )
+                          : Material(
+                              // display the new image upfate here....
+                              child: Image.file(
+                                imageFileAvatar,
+                                width: 200.0,
+                                height: 200.0,
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(125.0)),
+                              clipBehavior: Clip.hardEdge,
+                            ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.camera_alt,
+                          size: 100.0,
+                          color: Colors.white54.withOpacity(0.3),
+                        ),
+                        onPressed: getImage,
+                        padding: EdgeInsets.all(0.0),
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.grey,
+                        iconSize: 200.0,
+                      ),
+                    ],
                   ),
-                  imageUrl: photoUrl,
-                  width: 200.0,
-                  height: 200.0,
-                  fit: BoxFit.cover,
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(125.0)),
-                clipBehavior: Clip.hardEdge,
-              )
-              : Icon:(Icons.account_circle, size: 90.0, color: Colors.grey,)
-              : Material(
-                //displa the new image upfate here
-                child: Image.file(
-                  imageFileAvatar,
-                  with: 200.0,
-                  height:200.0,
-                  fit: BoxFit.cover,
-                ),
-
-
-
-
+                width: double.infinity,
+                margin: EdgeInsets.all(20.0),
               ),
-
-            ],
-          ),
-        ),
-
-    ),
             ],
           ),
         ),
